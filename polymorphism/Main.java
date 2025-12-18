7704e94ad98765a2dd1adb34418c50c77630bed7
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2025-12-18 14:49:23 +0530

    ploymo. done

---
+++ polymorphism/Main.java
@@ -0,0 +1,24 @@
+class Shape{
+    void draw(){
+        System.out.println("Drawing a shape");
+    }
+}
+class Circle extends Shape{
+    void draw(){
+        System.out.println("Drawing a circle");
+    }
+}
+class Rectangle extends Shape{
+    void draw(){  //overriding
+        System.out.println("Drawing a rectangle");
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Shape s;
+        s=new Circle();
+        s.draw();
+        s=new Rectangle();
+        s.draw();
+    }
+}
