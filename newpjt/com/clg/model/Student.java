dfc672032ccf3167b84ee022430de0a439c105e8
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2025-12-26 13:46:02 +0530

    Create Student.java

---
+++ newpjt/com/clg/model/Student.java
@@ -0,0 +1,13 @@
+package com.college.model;
+public class Student {
+    public String name;
+    public int age;
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
