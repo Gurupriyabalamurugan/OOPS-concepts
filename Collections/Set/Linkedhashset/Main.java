b76df13afb5ba4ba323a547aff6d2dbd36f82b26
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2026-01-02 11:07:47 +0530

    LinkedHashSet
    Demonstrates usage of LinkedHashSet to maintain insertion order.

---
+++ Collections/Set/Linkedhashset/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Set<Integer> set =new LinkedHashSet<>();
+        set.add(3);
+        set.add(1);
+        set.add(2);
+        System.out.println(set);
+    }
+}
+//has order of insersion
