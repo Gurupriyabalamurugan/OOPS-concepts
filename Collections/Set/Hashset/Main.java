6e124313b5b2364bae8324bafdf8a4ecacdfdf68
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2026-01-02 11:06:20 +0530

    HashSet

---
+++ Collections/Set/Hashset/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Java");
+        set.add("Java");//duplicate 
+        set.add("Python");
+        System.out.println(set);
+
+}
+}
