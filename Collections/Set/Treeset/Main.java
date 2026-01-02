4e5da3d75e9283d910f5b47f42c8ab9d00626f37
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2026-01-02 11:09:08 +0530

    TreeSet
    Implement a simple TreeSet example in Main.java

---
+++ Collections/Set/Treeset/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Set<Integer> set = new TreeSet<>();
+        set.add(30);
+        set.add(10);
+        set.add(20);
+        System.out.println(set); 
+    }
+}
+//elements r sorted
