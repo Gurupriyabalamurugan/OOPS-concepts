c9cc04a1bf156dc298356b15d4ceccc81a9d91e7
Gurupriya Balamurugan <152638209+Gurupriyabalamurugan@users.noreply.github.com>
2026-01-02 11:03:25 +0530

    LinkedList

---
+++ Collections/list/Linkedlist/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        List<Integer> list =new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(1, 15);
+        System.out.println(list);
+    }
+}
